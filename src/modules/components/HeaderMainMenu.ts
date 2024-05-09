ad7b490c1c882f0668e3590faeb669fad26138c7
Dmitrii Kushhenko <dikushch@gmail.com>
2024-05-10 00:01:21 +0300

    feat: add class for main menu in header

---
+++ src/modules/components/HeaderMainMenu.ts
@@ -0,0 +1,44 @@
+import BaseComponent from './BaseComponent';
+
+export default class HeaderMainMenu extends BaseComponent {
+  mainLink: BaseComponent;
+
+  catalogLink: BaseComponent;
+
+  aboutLink: BaseComponent;
+
+  constructor() {
+    super({ tag: 'nav', classes: ['header__nav', 'nav'] });
+    this.mainLink = new BaseComponent({
+      tag: 'a',
+      text: 'MAIN',
+      classes: ['nav__link'],
+    });
+    const mainLinkLi = new BaseComponent(
+      { tag: 'li', classes: ['nav__item'] },
+      this.mainLink,
+    );
+
+    this.catalogLink = new BaseComponent({
+      tag: 'a',
+      text: 'CATALOG',
+      classes: ['nav__link'],
+    });
+    const catalogLinkLi = new BaseComponent(
+      { tag: 'li', classes: ['nav__item'] },
+      this.catalogLink,
+    );
+
+    this.aboutLink = new BaseComponent({
+      tag: 'a',
+      text: 'ABOUT',
+      classes: ['nav__link'],
+    });
+    const aboutLinkLi = new BaseComponent(
+      { tag: 'li', classes: ['nav__item'] },
+      this.aboutLink,
+    );
+
+    this.appendChildren([mainLinkLi, catalogLinkLi, aboutLinkLi]);
+  }
+}
