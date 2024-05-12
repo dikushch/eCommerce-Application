d1cdd10a88a94111701c0b6f92de312864917a23
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-05-12 19:05:44 +0300

    feat: add address in registration layout

--- src/modules/components/RegForm.ts
+++ src/modules/components/RegForm.ts
@@ -2,16 +2,9 @@ import BaseComponent from './BaseComponent';
 import Input from './Input';
 import Button from './Button';
 import Label from './Label';
+import Select from './Select';
 
 export default class RegForm extends BaseComponent<HTMLFormElement> {
-  firstNameLabel: Label;
-
-  lastNameLabel: Label;
-
-  emailLabel: Label;
-
-  passwordLabel: Label;
-
   firstNameInput: Input;
 
   lastNameInput: Input;
@@ -19,6 +12,16 @@ export default class RegForm extends BaseComponent<HTMLFormElement> {
   emailInput: Input;
 
   passwordInput: Input;
+  // add address
+
+  streetInput: Input;
+
+  cityInput: Input;
+
+  postalCodeInput: Input;
+
+  // ???
+  countryInput: Select;
 
   submitBtn: Button;
 
@@ -26,9 +29,13 @@ export default class RegForm extends BaseComponent<HTMLFormElement> {
     super({ tag: 'form', classes: props.classes });
 
     // Initialize form
-    this.firstNameLabel = new Label({
+
+    const firstNameDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const firstNameLabel = new Label({
       forStr: 'firstNameInput',
-      text: 'Input First Name',
+      text: 'First Name',
       classes: ['label'],
     });
     this.firstNameInput = new Input({
@@ -36,10 +43,15 @@ export default class RegForm extends BaseComponent<HTMLFormElement> {
       classes: ['input'],
       id: 'firstNameInput',
     });
+    firstNameDiv.append(firstNameLabel);
+    firstNameDiv.append(this.firstNameInput);
 
-    this.lastNameLabel = new Label({
+    const lastNameDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const lastNameLabel = new Label({
       forStr: 'lastNameInput',
-      text: 'Input Last Name',
+      text: 'Last Name',
       classes: ['label'],
     });
     this.lastNameInput = new Input({
@@ -47,21 +59,31 @@ export default class RegForm extends BaseComponent<HTMLFormElement> {
       classes: ['input'],
       id: 'lastNameInput',
     });
+    lastNameDiv.append(lastNameLabel);
+    lastNameDiv.append(this.lastNameInput);
 
-    this.emailLabel = new Label({
+    const emailDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const emailLabel = new Label({
       forStr: 'emailInput',
-      text: 'Input Email',
+      text: 'Email',
       classes: ['label'],
     });
     this.emailInput = new Input({
-      type: 'email',
+      type: 'Email',
       classes: ['input'],
       id: 'emailInput',
     });
+    emailDiv.append(emailLabel);
+    emailDiv.append(this.emailInput);
 
-    this.passwordLabel = new Label({
+    const passwordDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const passwordLabel = new Label({
       forStr: 'passwordInput',
-      text: 'Input password',
+      text: 'Password',
       classes: ['label'],
     });
     this.passwordInput = new Input({
@@ -69,22 +91,115 @@ export default class RegForm extends BaseComponent<HTMLFormElement> {
       classes: ['input'],
       id: 'passwordInput',
     });
+    passwordDiv.append(passwordLabel);
+    passwordDiv.append(this.passwordInput);
 
     this.submitBtn = new Button({
       text: 'Register',
       classes: ['submit-btn'],
     });
 
+    // add container for reg data
+    const divRegist = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const divaddress = new BaseComponent({
+      classes: ['div-vert'],
+    });
+
+    // create address elements
+    const streetDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const streetLabel = new Label({
+      forStr: 'streetInput',
+      text: 'Street',
+      classes: ['label'],
+    });
+    this.streetInput = new Input({
+      type: 'text',
+      classes: ['input'],
+      id: 'streetInput',
+    });
+    streetDiv.append(streetLabel);
+    streetDiv.append(this.streetInput);
+
+    const cityDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const cityLabel = new Label({
+      forStr: 'cityInput',
+      text: 'City',
+      classes: ['label'],
+    });
+    this.cityInput = new Input({
+      type: 'text',
+      classes: ['input'],
+      id: 'cityInput',
+    });
+    cityDiv.append(cityLabel);
+    cityDiv.append(this.cityInput);
+
+    const postalCodeDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const postalCodeLabel = new Label({
+      forStr: 'postalCodeInput',
+      text: 'Postal Code',
+      classes: ['label'],
+    });
+    this.postalCodeInput = new Input({
+      type: 'text',
+      classes: ['input'],
+      id: 'postalCodeInput',
+    });
+    postalCodeDiv.append(postalCodeLabel);
+    postalCodeDiv.append(this.postalCodeInput);
+
+    const countryDiv = new BaseComponent({
+      classes: ['div-vert'],
+    });
+    const countryLabel = new Label({
+      forStr: 'country',
+      text: 'Country',
+      classes: ['label'],
+    });
+
+    const countryOptions = [
+      { value: 'us', text: 'United States' },
+      { value: 'au', text: 'Australia' },
+    ];
+
+    // Create an instance of the Selector with the options
+    /* const countrySelector = new Select(countryOptions, 'Select Country', {
+      classes: ['county-selrector'],
+    }); */
+    this.countryInput = new Select(countryOptions, {
+      classes: ['country-selector'],
+      id: 'country',
+    });
+    countryDiv.append(countryLabel);
+    countryDiv.append(this.countryInput);
+
     // Append the form elements to the form
 
-    this.append(this.firstNameLabel);
-    this.append(this.firstNameInput);
-    this.append(this.lastNameLabel);
-    this.append(this.lastNameInput);
-    this.append(this.emailLabel);
-    this.append(this.emailInput);
-    this.append(this.passwordLabel);
-    this.append(this.passwordInput);
+    divRegist.append(firstNameDiv);
+    divRegist.append(lastNameDiv);
+    divRegist.append(emailDiv);
+    divRegist.append(passwordDiv);
+
+    divaddress.append(streetDiv);
+    divaddress.append(cityDiv);
+    divaddress.append(postalCodeDiv);
+    divaddress.append(countryDiv);
+
+    const divInputsContainer = new BaseComponent({
+      classes: ['div-hor'],
+    });
+
+    divInputsContainer.append(divRegist);
+    divInputsContainer.append(divaddress);
+    this.append(divInputsContainer);
     this.append(this.submitBtn);
   }
 }
