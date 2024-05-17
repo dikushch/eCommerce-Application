00603f0f05e2ca942b4977d277cad55a7204f361
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-17 13:26:00 +0300

    feat: add types for api functions

--- src/modules/types/Types.ts
+++ src/modules/types/Types.ts
@@ -2,3 +2,34 @@ export interface RouteItem {
   path: string;
   component: HTMLElement;
 }
+
+export interface TokenResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  refresh_token?: string;
+}
+
+export interface CustomerAddress {
+  country: 'AU' | 'US';
+  city: string;
+  streetName: string;
+  postalCode: string;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface NewCustomerData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  dateOfBirth?: string;
+  addresses: CustomerAddress[];
+  defaultShippingAddress?: number;
+  defaultBillingAddress?: number;
+}
