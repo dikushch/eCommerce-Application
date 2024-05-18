800e6d94e8cdc3e9fc4a788d7b815e2310ea207e
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-18 19:32:55 +0300

    feat: add validation for email and password

--- src/modules/pages/LoginPage.ts
+++ src/modules/pages/LoginPage.ts
@@ -12,6 +12,10 @@ export default class LoginPage extends BaseComponent {
 
   submit: Button;
 
+  isEmailValid: boolean = false;
+
+  isPassValid: boolean = false;
+
   constructor() {
     super({ tag: 'section', classes: ['login', 'container'] });
     this.form = new LoginForm();
@@ -21,5 +25,47 @@ export default class LoginPage extends BaseComponent {
     this.submit.disable();
 
     this.append(this.form);
+
+    this.email.addListener('input', () => {
+      this.isEmailValid = this.checkEmail();
+      this.checkValidForm();
+    });
+
+    this.password.addListener('input', () => {
+      this.isPassValid = this.checkPass();
+      this.checkValidForm();
+    });
+  }
+
+  checkValidForm(): void {
+    if (this.isEmailValid && this.isPassValid) {
+      this.submit.enable();
+    } else {
+      this.submit.disable();
+    }
+  }
+
+  checkEmail(): boolean {
+    const regExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const value = this.email.getValue();
+    if (regExp.test(value)) {
+      this.email.removeClass('wrong');
+      return true;
+    }
+
+    this.email.addClass('wrong');
+    return false;
+  }
+
+  checkPass(): boolean {
+    const regExp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\w\s])[^\s]{8,}$/;
+    const value = this.password.getValue();
+    if (regExp.test(value)) {
+      this.password.removeClass('wrong');
+      return true;
+    }
+
+    this.password.addClass('wrong');
+    return false;
   }
 }
