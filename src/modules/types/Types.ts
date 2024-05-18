32f7246167db3fe84b2db0d088c35d426d7ff71f
dikushch <33037100+dikushch@users.noreply.github.com>
2024-05-18 17:28:37 +0300

    feat: add api functions

--- src/modules/types/Types.ts
+++ src/modules/types/Types.ts
@@ -2,3 +2,65 @@ export interface RouteItem {
   path: string;
   component: HTMLElement;
 }
+
+export interface TokenResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  refresh_token?: string;
+}
+
+export interface CustomerAddress {
+  country: 'AU' | 'US';
+  city: string;
+  streetName: string;
+  postalCode: string;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface NewCustomerData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  dateOfBirth?: string;
+  addresses: CustomerAddress[];
+  defaultShippingAddress?: number;
+  defaultBillingAddress?: number;
+}
+
+export interface CustomerLoginResponse {
+  customer: Customer;
+}
+
+interface Customer {
+  id: string;
+  version: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  isEmailVerified: boolean;
+  createdAt: string;
+  lastModifiedAt: string;
+  addresses: Address[];
+  defaultShippingAddressId: string;
+  shippingAddressIds: string[];
+  defaultBillingAddressId: string;
+  billingAddressIds: string[];
+  authenticationMode: string;
+  stores: [];
+}
+
+interface Address {
+  id: string;
+  streetName: string;
+  postalCode: string;
+  city: string;
+  country: string;
+}
