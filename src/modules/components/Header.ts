ef566ea52427bd391244146c20d06ac1595478ae
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-18 13:27:18 +0300

    feat: add header functionality

--- src/modules/components/Header.ts
+++ src/modules/components/Header.ts
@@ -10,11 +10,13 @@ export default class Header extends BaseComponent {
 
   brg: BaseComponent;
 
-  constructor() {
+  links: HTMLElement[];
+
+  constructor(isLogin: boolean) {
     super({ tag: 'heeader', classes: ['header', 'container'] });
     const logo = new HeaderLogo();
     this.mainMenu = new HeaderMainMenu();
-    this.userMenu = new HeaderUserMenu();
+    this.userMenu = new HeaderUserMenu(isLogin);
     this.brg = new BaseComponent({ classes: ['brg'] });
 
     this.append(logo);
@@ -22,8 +24,29 @@ export default class Header extends BaseComponent {
     this.append(this.userMenu);
     this.append(this.brg);
 
+    this.links = [
+      this.mainMenu.mainLink.getNode(),
+      this.mainMenu.catalogLink.getNode(),
+      this.mainMenu.aboutLink.getNode(),
+      this.userMenu.cart.getNode(),
+      this.userMenu.login.getNode(),
+      this.userMenu.register.getNode(),
+      this.userMenu.user.getNode(),
+      this.userMenu.logout.getNode(),
+    ];
+
     this.brg.addListener('click', () => {
       this.openMenu();
+    });
+
+    this.addListener('click', (e) => {
+      if (e.target instanceof HTMLElement) {
+        if (this.links.includes(e.target)) {
+          console.log(e.target);
+          this.setActiveLink(e.target);
+          this.dispatchChangePageEvent(e.target);
+        }
+      }
     });
   }
 
@@ -31,5 +54,34 @@ export default class Header extends BaseComponent {
     this.brg.toggleClass('open');
     this.mainMenu.toggleClass('open');
     this.userMenu.toggleClass('open');
+  }
+
+  setActiveLink(link: HTMLElement): void {
+    const clickedLink = this.links.find((l) => l === link);
+    if (!clickedLink?.classList.contains('active')) {
+      this.clearActiveClass();
+      clickedLink?.classList.add('active');
+      this.closeBrg();
+    }
+  }
+
+  clearActiveClass(): void {
+    this.links.forEach((l) => {
+      l.classList.remove('active');
+    });
+  }
+
+  closeBrg(): void {
+    this.brg.removeClass('open');
+    this.mainMenu.removeClass('open');
+    this.userMenu.removeClass('open');
+  }
+
+  dispatchChangePageEvent(link: HTMLElement): void {
+    const event = new CustomEvent('change-page', {
+      bubbles: true,
+      detail: link.dataset.href,
+    });
+    this.getNode().dispatchEvent(event);
   }
 }
