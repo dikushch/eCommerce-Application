ec9425af615590a87f0ba8c40be443d270a33c33
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-18 22:39:53 +0300

    feat: add App class with routing

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,65 @@
+import Header from './modules/components/Header';
+import LoginPage from './modules/pages/LoginPage';
+import MainPage from './modules/pages/MainPage';
+import NotFoundPage from './modules/pages/NotFoundPage';
+import Router from './modules/router/Router';
+import { RouteItem } from './modules/types/Types';
 import './styles.scss';
+
+class App {
+  element: HTMLElement = document.body;
+
+  router: Router;
+
+  header: Header;
+
+  main: MainPage;
+
+  login: LoginPage;
+
+  notFoundPage: NotFoundPage;
+
+  constructor() {
+    this.header = new Header(false);
+    this.element.append(this.header.getNode());
+    this.notFoundPage = new NotFoundPage();
+    this.main = new MainPage();
+    this.login = new LoginPage();
+    const routes: RouteItem[] = [
+      { path: '/', component: this.main.getNode() },
+      { path: '/login', component: this.login.getNode() },
+      { path: '/404', component: this.notFoundPage.getNode() },
+    ];
+    this.router = new Router(routes);
+    this.setMenuItemActive(window.location.pathname);
+  }
+
+  setMenuItemActive(name: string): void {
+    let linkName;
+    if (name === '/') {
+      linkName = 'main';
+    } else {
+      linkName = name.slice(1);
+    }
+    const link = this.header.findLink(linkName);
+    if (link) {
+      this.header.setActiveLink(link);
+    }
+  }
+
+  changePageHandler(e: CustomEvent) {
+    const path = (e as CustomEvent).detail;
+    console.log(path);
+    this.setMenuItemActive(path);
+    this.router.changeRoute(path);
+  }
+
+  addListeners() {
+    this.element.addEventListener('change-page', (e) => {
+      this.changePageHandler(e as CustomEvent);
+    });
+  }
+}
+
+const app = new App();
+app.addListeners();
