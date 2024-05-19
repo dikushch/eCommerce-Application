7b7959da6f52d0221119d46378a33f6bc6d94c7d
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-19 19:37:55 +0300

    feat: add all links to pages on main page

--- src/modules/pages/MainPage.ts
+++ src/modules/pages/MainPage.ts
@@ -1,12 +1,18 @@
+import AllLinks from '../components/AllLinks';
 import BaseComponent from '../components/BaseComponent';
 import Button from '../components/Button';
 
 export default class MainPage extends BaseComponent {
+  allLinks: AllLinks;
+
   constructor() {
     super({
       tag: 'section',
       classes: ['main', 'container'],
     });
+
+    this.allLinks = new AllLinks();
+    this.append(this.allLinks);
 
     const imageContainer = new BaseComponent({
       tag: 'div',
