877b0ca5671137307a98d25102e93272684c9ac6
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-05-19 16:20:13 +0300

    fix: delete app.ts for PR

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,1 @@
-import Header from './modules/components/Header';
-import RegistrationPage from './modules/pages/RegPage';
 import './styles.scss';
-
-const headPage = new Header();
-const regPage = new RegistrationPage();
-
-document.body.append(headPage.getNode());
-document.body.append(regPage.getNode());
