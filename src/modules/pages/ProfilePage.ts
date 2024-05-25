dedf3f4822bff93fa55acbf48f022528c77b7220
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-05-25 18:09:43 +0300

    feat: add address block

--- src/modules/pages/ProfilePage.ts
+++ src/modules/pages/ProfilePage.ts
@@ -1,9 +1,12 @@
 import BaseComponent from '../components/BaseComponent';
 import ProfileInfo from '../components/ProfileInfo';
+import ProfileAddressBlock from '../components/ProfileAddressBlock';
 import { Customer } from '../types/Types';
 
 export default class ProfilePage extends BaseComponent {
   profileInfo: ProfileInfo;
+
+  profileAddressBlock: ProfileAddressBlock;
 
   userInfo: Customer;
 
@@ -16,9 +19,11 @@ export default class ProfilePage extends BaseComponent {
 
     // personal information
     this.profileInfo = new ProfileInfo(userInfo);
+    this.profileAddressBlock = new ProfileAddressBlock(userInfo);
 
     // console.log(userInfo);
 
     this.append(this.profileInfo);
+    this.append(this.profileAddressBlock);
   }
 }
