9fd0b683c0f33cfbe96b25d8d41c5ac9eb009bcf
Kenjaeff <149934082+Kenjaeff@users.noreply.github.com>
2024-05-26 12:41:02 +0500

    feat: add product info

--- src/modules/pages/ProductPage.ts
+++ src/modules/pages/ProductPage.ts
@@ -10,12 +10,83 @@ export default class ProductPage extends BaseComponent {
   discount?: number;
 
   constructor(name: string, descr: string, price: number, discount?: number) {
-    super({});
+    super({
+      tag: 'section',
+      classes: ['product-page', 'container'],
+    });
     this.name = name;
     this.descr = descr;
     this.price = price;
     if (discount) {
       this.discount = discount;
     }
+
+    const productContainer = new BaseComponent({
+      tag: 'div',
+      classes: ['product-container'],
+    });
+
+    const productName = new BaseComponent({
+      tag: 'h1',
+      text: this.name,
+      classes: ['product-name'],
+    });
+
+    const productDescription = new BaseComponent({
+      tag: 'p',
+      text: this.descr,
+      classes: ['product-description'],
+    });
+
+    const priceContainer = new BaseComponent({
+      tag: 'div',
+      classes: ['price-container'],
+    });
+
+    const originalPrice = new BaseComponent({
+      tag: 'span',
+      text: `${this.price}$`,
+      classes: ['original-price'],
+    });
+
+    let salePrice: BaseComponent<HTMLElement> | null = null;
+    let saleLabel: BaseComponent<HTMLElement> | null = null;
+
+    if (this.discount) {
+      const saleWrapper = new BaseComponent({
+        tag: 'div',
+        classes: ['sale-wrapper'],
+      });
+
+      saleLabel = new BaseComponent({
+        tag: 'span',
+        text: 'sale',
+        classes: ['sale-label'],
+      });
+
+      salePrice = new BaseComponent({
+        tag: 'span',
+        text: `${this.discount}$`,
+        classes: ['sale-price'],
+      });
+
+      saleWrapper.append(saleLabel);
+      saleWrapper.append(salePrice);
+      priceContainer.append(saleWrapper);
+    }
+
+    priceContainer.append(originalPrice);
+    productContainer.append(productName);
+    productContainer.append(productDescription);
+    productContainer.append(priceContainer);
+
+    const addToCartButton = new BaseComponent({
+      tag: 'button',
+      text: 'add to cart',
+      classes: ['add-to-cart'],
+    });
+
+    productContainer.append(addToCartButton);
+    this.append(productContainer);
   }
 }
