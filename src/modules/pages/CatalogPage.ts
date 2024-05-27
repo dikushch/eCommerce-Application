35d4d3e0996906146a21393637472847e99fdb50
Dmitrii Kushchenko <dikushch@gmail.com>
2024-05-27 22:14:59 +0300

    feat: add Catalog page layout and styles for it

--- src/modules/pages/CatalogPage.ts
+++ src/modules/pages/CatalogPage.ts
@@ -1,7 +1,34 @@
 import BaseComponent from '../components/BaseComponent';
+import CatalogFiltersSorts from '../components/CatalogFiltersSorts';
+import CatalogList from '../components/CatalogList';
+import CatalogProductTypes from '../components/CatalogProductsTypes';
+import { ProductsResponse, SearchProductsData } from '../types/Types';
 
 export default class CatalogPage extends BaseComponent {
+  catalogList: CatalogList | null = null;
+
+  types: CatalogProductTypes;
+
+  query: CatalogFiltersSorts;
+
   constructor() {
-    super({});
+    super({ tag: 'section', classes: ['catalog', 'container'] });
+    this.types = new CatalogProductTypes();
+    this.query = new CatalogFiltersSorts();
+    this.append(this.types);
+    this.append(this.query);
+  }
+
+  createProductsList(data: ProductsResponse): void {
+    this.catalogList = new CatalogList(data);
+    this.append(this.catalogList);
+  }
+
+  dispatchUpdateCatalogEvent(searchParams: SearchProductsData): void {
+    const event = new CustomEvent('update-catalog', {
+      bubbles: true,
+      detail: searchParams,
+    });
+    this.getNode().dispatchEvent(event);
   }
 }
