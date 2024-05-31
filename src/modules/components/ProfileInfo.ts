ee684c264f91aae146ff1878056b56db67d8bea7
Dmitry Boris <boris.dmitriy00@mail.ru>
2024-05-31 10:52:35 +0300

    feat: add validation for inputs

--- src/modules/components/ProfileInfo.ts
+++ src/modules/components/ProfileInfo.ts
@@ -21,8 +21,17 @@ export default class ProfileInfo extends BaseComponent {
 
   isEditMode: boolean = false;
 
+  isValidInputs: boolean = true;
+
   constructor(userInfo: Customer) {
     super({ classes: ['profile__info'] });
+
+    const spanError = (textExample: string) =>
+      new BaseComponent({
+        tag: 'span',
+        classes: ['reg_form_error-hide'],
+        text: `Incorrect input, example: ${textExample}`,
+      });
 
     const firstNameLabel = new Label({
       forStr: 'profileFirstNameInput',
@@ -37,6 +46,7 @@ export default class ProfileInfo extends BaseComponent {
         classes: ['profile__box-input'],
         id: 'profileFirstNameInput',
       })),
+      spanError('Bob'),
     );
 
     const lastNameLabel = new Label({
@@ -52,6 +62,7 @@ export default class ProfileInfo extends BaseComponent {
         classes: ['profile__box-input'],
         id: 'profileLastNameInput',
       })),
+      spanError('Martin'),
     );
 
     const birthDateLabel = new Label({
@@ -67,6 +78,7 @@ export default class ProfileInfo extends BaseComponent {
         classes: ['profile__box-input'],
         id: 'profileBirthDateInput',
       })),
+      spanError('must be at least 13 years old'),
     );
 
     const emailLabel = new Label({
@@ -82,6 +94,7 @@ export default class ProfileInfo extends BaseComponent {
         classes: ['profile__box-input'],
         id: 'profileEmailInput',
       })),
+      spanError('example@exam.ex'),
     );
 
     const profileEditBtnDiv = new BaseComponent(
@@ -97,7 +110,12 @@ export default class ProfileInfo extends BaseComponent {
       if (!this.isEditMode) {
         this.initEditMode();
       } else {
-        this.prepareDataToRequest(userInfo);
+        this.isValidInputs = true;
+        this.checkAllInputsValue();
+
+        if (this.isValidInputs) {
+          this.prepareDataToRequest(userInfo);
+        }
       }
     });
 
@@ -108,6 +126,7 @@ export default class ProfileInfo extends BaseComponent {
         classes: ['profile__box-btn', 'red'],
       })),
     );
+
     // add listeners
     this.profileChangePasswordBtn.addListener('click', () => {
       console.log('click change password btn');
@@ -116,11 +135,22 @@ export default class ProfileInfo extends BaseComponent {
       }
     });
 
+    // clean error message
+    this.profileFirstNameInput.addListener('input', () => {
+      ProfileInfo.removeIncorrectStyle(this.profileFirstNameInput);
+    });
+    this.profileLastNameInput.addListener('input', () => {
+      ProfileInfo.removeIncorrectStyle(this.profileLastNameInput);
+    });
+    this.profileBirthDateInput.addListener('input', () => {
+      ProfileInfo.removeIncorrectStyle(this.profileBirthDateInput);
+    });
+    this.profileEmailInput.addListener('input', () => {
+      ProfileInfo.removeIncorrectStyle(this.profileEmailInput);
+    });
+
     // disable all inputs
-    this.profileFirstNameInput.disable();
-    this.profileLastNameInput.disable();
-    this.profileBirthDateInput.disable();
-    this.profileEmailInput.disable();
+    this.changeInputStatus(false);
 
     // set value
     this.profileFirstNameInput.setValue(userInfo.firstName);
@@ -160,10 +190,7 @@ export default class ProfileInfo extends BaseComponent {
   initEditMode() {
     this.isEditMode = true;
 
-    this.profileFirstNameInput.enable();
-    this.profileLastNameInput.enable();
-    this.profileEmailInput.enable();
-    this.profileBirthDateInput.enable();
+    this.changeInputStatus(true);
 
     this.profileEditBtn.setTextContent('save');
     this.profileChangePasswordBtn.setTextContent('cancel');
@@ -174,13 +201,29 @@ export default class ProfileInfo extends BaseComponent {
 
     this.cancelInputChanges(userInfo);
 
-    this.profileFirstNameInput.disable();
-    this.profileLastNameInput.disable();
-    this.profileEmailInput.disable();
-    this.profileBirthDateInput.disable();
+    this.changeInputStatus(false);
+
+    ProfileInfo.removeIncorrectStyle(this.profileFirstNameInput);
+    ProfileInfo.removeIncorrectStyle(this.profileLastNameInput);
+    ProfileInfo.removeIncorrectStyle(this.profileBirthDateInput);
+    ProfileInfo.removeIncorrectStyle(this.profileEmailInput);
 
     this.profileEditBtn.setTextContent('edit profile');
     this.profileChangePasswordBtn.setTextContent('change password');
+  }
+
+  changeInputStatus(isEnable: boolean) {
+    if (isEnable) {
+      this.profileFirstNameInput.enable();
+      this.profileLastNameInput.enable();
+      this.profileEmailInput.enable();
+      this.profileBirthDateInput.enable();
+    } else {
+      this.profileFirstNameInput.disable();
+      this.profileLastNameInput.disable();
+      this.profileEmailInput.disable();
+      this.profileBirthDateInput.disable();
+    }
   }
 
   cancelInputChanges(userInfo: Customer) {
@@ -188,6 +231,102 @@ export default class ProfileInfo extends BaseComponent {
     this.profileLastNameInput.setValue(userInfo.lastName);
     this.profileEmailInput.setValue(userInfo.email);
     this.profileBirthDateInput.setValue(userInfo.dateOfBirth);
+  }
+
+  checkAllInputsValue() {
+    const regSingleWordExp = /^[A-Za-z]+$/;
+    const regMailExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const arrayValuesAndRegExp: [Input, RegExp][] = [
+      [this.profileFirstNameInput, regSingleWordExp],
+      [this.profileLastNameInput, regSingleWordExp],
+      [this.profileEmailInput, regMailExp],
+    ];
+
+    if (!ProfileInfo.validateInputDateValue(this.profileBirthDateInput)) {
+      this.isValidInputs = false;
+    }
+    arrayValuesAndRegExp.forEach((valueAndReg) => {
+      if (!ProfileInfo.validateInputValue(valueAndReg[0], valueAndReg[1])) {
+        this.isValidInputs = false;
+      }
+    });
+  }
+
+  static validateInputValue(inputValue: Input, regExp: RegExp) {
+    const spanError: Element | null = inputValue.getNode().nextElementSibling;
+    if (regExp.test(inputValue.getValue())) {
+      inputValue.removeClass('incorrect_input');
+      inputValue.addClass('correct_input');
+      if (spanError) {
+        spanError.classList.remove('reg_form_error-show');
+        spanError.classList.add('reg_form_error-hide');
+      }
+      return true;
+    }
+    inputValue.removeClass('correct_input');
+    inputValue.addClass('incorrect_input');
+
+    if (spanError) {
+      spanError.classList.remove('reg_form_error-hide');
+      spanError.classList.add('reg_form_error-show');
+    }
+    return false;
+  }
+
+  static validateInputDateValue(inputValue: Input) {
+    const spanError: Element | null = inputValue.getNode().nextElementSibling;
+
+    const minimumAge = 13;
+    const birthDate = new Date(inputValue.getValue());
+
+    const currentDate = new Date();
+
+    // Calculate the difference in milliseconds
+    const differenceInMilliseconds =
+      currentDate.getTime() - birthDate.getTime();
+
+    // Convert milliseconds to years
+    const ageInYears =
+      differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+
+    // Check if age is at least the minimum required age
+
+    if (ageInYears >= minimumAge) {
+      inputValue.removeClass('incorrect_input');
+      inputValue.addClass('correct_input');
+
+      if (spanError) {
+        spanError.classList.remove('reg_form_error-show');
+        spanError.classList.add('reg_form_error-hide');
+      }
+      return true;
+    }
+    // if false show error message
+    inputValue.removeClass('correct_input');
+    inputValue.addClass('incorrect_input');
+
+    if (spanError) {
+      spanError.classList.remove('reg_form_error-hide');
+      spanError.classList.add('reg_form_error-show');
+    }
+    return false;
+  }
+
+  static removeIncorrectStyle(node: Input) {
+    if (
+      node
+        .getNode()
+        .nextElementSibling?.classList.contains('reg_form_error-show')
+    ) {
+      node.removeClass('incorrect_input');
+
+      node.getNode().nextElementSibling?.classList.add('reg_form_error-hide');
+
+      node
+        .getNode()
+        .nextElementSibling?.classList.remove('reg_form_error-show');
+    }
   }
 
   prepareDataToRequest(userInfo: Customer) {
@@ -213,7 +352,9 @@ export default class ProfileInfo extends BaseComponent {
       ],
     };
 
-    this.dispathUpdateEvent(userInfo.id, updateUserInfo);
+    console.log(updateUserInfo);
+
+    // this.dispathUpdateEvent(userInfo.id, updateUserInfo);
   }
 
   dispathUpdateEvent(id: string, data: ChangeCustomerRequest): void {
