19693c60a77004a8e94a5c43663ad4021d5e5e97
Dmitry Boris <36541816+BorisDmitriy@users.noreply.github.com>
2024-06-11 20:46:56 +0300

    Feat/add cart layout (#41)
    * feat: create cart product block
    
    * feat: add styles for cart
    
    * feat: implement product block on cart page
    
    * fix: change class name in CartProductBlock.ts
    
    * fix: change class name for h3

--- src/modules/pages/CartPage.ts
+++ src/modules/pages/CartPage.ts
@@ -1,13 +1,27 @@
 import BaseComponent from '../components/BaseComponent';
+import CartProductBlock from '../components/CartProductBlock';
 import { Cart } from '../types/Types';
 
 export default class CartPage extends BaseComponent {
   cartData?: Cart;
 
   constructor(cartData?: Cart) {
-    super({});
+    super({ tag: 'section', classes: ['cart', 'container'] });
+    const cartItemsDiv = new BaseComponent({
+      classes: ['cart-items'],
+    });
+
     if (cartData) {
       this.cartData = cartData;
+
+      // add all items in div
+      cartData.lineItems.forEach((item) => {
+        cartItemsDiv.appendChildren([new CartProductBlock(item)]);
+        // console.log(item);
+      });
+
+      // add on page
+      this.append(cartItemsDiv);
     }
   }
 }
