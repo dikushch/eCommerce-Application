6fce8439007fc17b8b9c89beadb29602685ed8bc
Kenjaeff <149934082+Kenjaeff@users.noreply.github.com>
2024-06-14 22:13:46 +0500

    Feat/about card (#49)
    * feat: add about card
    
    * feat: add some styles

--- src/modules/pages/AboutPage.ts
+++ src/modules/pages/AboutPage.ts
@@ -1,7 +1,18 @@
 import BaseComponent from '../components/BaseComponent';
+import AboutCard from '../components/AboutCard';
 
 export default class AboutPage extends BaseComponent {
   constructor() {
     super({ tag: 'section', classes: ['about', 'container'] });
+
+    const aboutCard = new AboutCard({
+      name: 'Dmitrii Kushchenko',
+      githubUrl: 'https://github.com/dikushch',
+      role: 'developer',
+      school: 'Student of the RSSchool',
+      works: ['login page', 'catalog page', 'work with Api'],
+    });
+
+    this.append(aboutCard);
   }
 }
